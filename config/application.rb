c206fa5497ce7a68fa66d82ff5e434e31948c96c
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-08-05 10:39:11 +0900

    Fix: 余分な空行を削除

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module YouWillNeverWalkAlone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.assets false　#assetsを生成しない
+      g.helper false　#helperを生成しない
+      g.test_framework false　#testファイルを生成しない
+      g.skip_routes true　#ルーティングを生成しない
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
