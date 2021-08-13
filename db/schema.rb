db964d16b95410ceeb0cedc4ff3c733b307df33b
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-08-13 19:17:26 +0900

    Remove: gem deviseでUserモデルを作成する為、既に作成していUser関連のファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_044731) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
