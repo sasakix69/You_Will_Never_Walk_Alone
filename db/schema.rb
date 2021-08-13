becfc0248a734e145b3a1af65d1c7f36d93777d8
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-08-13 22:48:51 +0900

    fix: Twitterでのログインをできる様にomniauth-twitter用カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_102200) do
+ActiveRecord::Schema.define(version: 2021_08_13_134329) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2021_08_13_102200) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
