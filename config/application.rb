8e9fdbcc5b2bc236c7814dc1e6f44bd93aa00c55
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-08-21 18:28:09 +0900

    Add: デフォルトのロケールを日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module YouWillNeverWalkAlone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja # 日本語に国際化
 
     config.generators do |g|
       g.skip_routes true
