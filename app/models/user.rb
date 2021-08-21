414d55cf6e1352b8fba56859ef778080ff0c194b
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-08-21 15:43:21 +0900

    Add: omniauthでtwitterが使えるように設定を追加し、Twitter認証でログインした際に、 はじめてのログインの場合は、Twitterの情報を元に新しくユーザーを登録する。 すでに作成済みの場合は、Userテーブルから同じデータを持ってくる。という処理を記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,32 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:twitter]
+
+  # Twitter認証ログイン用
+  # ユーザーの情報があれば探し、無ければ作成する
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+      
+    unless user
+      user = User.create(
+              uid:      auth.uid,
+              provider: auth.provider,
+              name: auth.info.name,
+              username: auth.info.username,
+              email:    User.dummy_email(auth),
+              password: Devise.friendly_token[0, 20]
+            )
+            user.save!
+    end
+      
+    current_user = user
+  end
+      
+  private
+
+  # ダミーのメールアドレスを作成
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
