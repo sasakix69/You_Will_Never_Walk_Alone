b9831caaaf36db53f8a09989a6b42f9eae19e53e
rq-sasakix69-at-321899771753 <sasakix69@gmail.com>
2021-09-01 16:25:49 +0900

    Add: パラメーターにプロフィールの項目であるカラムを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # ログイン済ユーザーのみにアクセスを許可する
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index]
 
   protected
 
   # :account_update を記述して、新規登録のときは必要ないが、あとからプロフィール充実させるときは許可したいカラム名をここに追記
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image avatar favorite_player kop_history])
   end
 end
